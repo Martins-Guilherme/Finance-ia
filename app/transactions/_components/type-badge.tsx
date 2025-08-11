22e2ed7a453c4af5610fd9fab69ec89f42f4bb94
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2025-08-11 07:17:52 -0300

    feat: add transaction type badge

---
+++ app/transactions/_components/type-badge.tsx
@@ -0,0 +1,34 @@
+import { Transaction, TransactionType } from "@prisma/client";
+import { Badge } from "@/app/_components/ui/badge";
+import { CircleIcon } from "lucide-react";
+
+interface TransactionTypeBadgeProps {
+  transaction: Transaction;
+}
+
+const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
+  if (transaction.type === TransactionType.DEPOSIT) {
+    return (
+      <Badge className="bg-muted font-bold text-primary hover:bg-muted">
+        <CircleIcon className="mr-2 fill-primary" size={10} />
+        Depósito
+      </Badge>
+    );
+  }
+  if (transaction.type === TransactionType.EXPENSE) {
+    return (
+      <Badge className="bg-muted text-danger hover:bg-muted">
+        <CircleIcon className="mr-2 fill-danger" size={10} />
+        Despesa
+      </Badge>
+    );
+  }
+  return (
+    <Badge className="bg-muted font-bold text-white hover:bg-muted">
+      <CircleIcon className="mr-2 fill-white" size={10} />
+      Ivestimento
+    </Badge>
+  );
+};
+
+export default TransactionTypeBadge;
--- app/transations/page.tsx
+++
@@ -1,9 +0,0 @@
-const TransitionsPage = () => {
-  return (
-    <>
-      <h1>Transations page</h1>
-    </>
-  );
-};
-
-export default TransitionsPage;

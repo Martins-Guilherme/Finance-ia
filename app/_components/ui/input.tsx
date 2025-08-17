2a5bd583847984f26ca3c8093810f02e647c6348
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2025-08-17 15:18:28 -0300

    fix: display height "dialog" and component 'input' fixed

--- app/_components/ui/input.tsx
+++ app/_components/ui/input.tsx
@@ -1,10 +1,9 @@
 import * as React from "react";
-
 import { cn } from "@/app/_lib/utils";
-
-export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
-
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
@@ -20,5 +19,4 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
   },
 );
 Input.displayName = "Input";
-
 export { Input };

5512a6e8cb7931174b6f0de280e0dffc0efbe74d
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2025-08-17 21:46:39 -0300

    feat: ensure transaction and subscription pages are protected

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,11 +1,22 @@
+import { auth } from "@clerk/nextjs/server";
 import AddTransactionButton from "../_components/add-transactions-button";
 import Navbar from "../_components/navbar";
 import { DataTable } from "../_components/ui/data-table";
 import { db } from "../_lib/prisma";
 import { transactionsColumns } from "./_columns";
+import { redirect } from "next/navigation";
 
 const TransitionsPage = async () => {
-  const transactions = await db.transaction.findMany({});
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  // BUSCAR AS TRANSAÇÕES NO BANCO DE DADOS
+  const transactions = await db.transaction.findMany({
+    where: {
+      userId,
+    },
+  });
   return (
     <>
       <Navbar />

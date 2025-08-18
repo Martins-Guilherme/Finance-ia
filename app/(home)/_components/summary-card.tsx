699af0e0f3e0323c924f4cba9926260bf47bf3eb
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2025-08-17 23:41:20 -0300

    feat: add summary cards

---
+++ app/(home)/_components/summary-card.tsx
@@ -0,0 +1,49 @@
+import AddTransactionButton from "@/app/_components/add-transactions-button";
+import { Card, CardContent, CardHeader } from "@/app/_components/ui/card";
+import { ReactNode } from "react";
+
+interface SummaryCardProps {
+  icon: ReactNode;
+  title: string;
+  amount: number;
+  size?: "small" | "large";
+}
+
+const SummaryCard = ({
+  icon,
+  title,
+  amount,
+  size = "small",
+}: SummaryCardProps) => {
+  return (
+    <>
+      <Card className="flex-row items-center gap-2">
+        <CardHeader>
+          {icon}
+          <p
+            className={
+              size === "small"
+                ? "text-muted-foreground opacity-70"
+                : "text-white opacity-70"
+            }
+          >
+            {title}
+          </p>
+        </CardHeader>
+        <CardContent className="flex justify-between">
+          <p
+            className={`font-bold ${size === "small" ? "text-2xl" : "text-4xl"}`}
+          >
+            {Intl.NumberFormat("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            }).format(amount)}
+          </p>
+          {size === "large" && <AddTransactionButton />}
+        </CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default SummaryCard;
